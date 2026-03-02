b82df759911d9fec95c702f731fb405436753f1b
Aravinth <itc006@email.com>
2026-03-02 15:12:15 +0530

    personal portfolio

---
+++ src/config.ts
@@ -0,0 +1,122 @@
+export const siteConfig = {
+  name: "Aravinth T",
+  title: "Full Stack Web Developer",
+  description: "Portfolio website of Ryan Fitzgerald",
+  accentColor: "#4361EE",
+  social: {
+    email: "aravintht721@gmail.com",
+    linkedin: "https://www.linkedin.com/in/aravinth-webdeveloper/",
+    whatsapp: "https://wa.me/916381216369",
+    github: "https://github.com/TAravinth-tech",
+  },
+  aboutMe:
+    "I’m Aravinth, a passionate Web Developer who is eager to learn new technologies and continuously improve my skills in modern web development. I enjoy building scalable and efficient web applications while continuously learning new technologies. My goal is to work in a technology-driven organization where I can grow as a developer and contribute to creating impactful solutions for society.",
+  skills: ["Javascript", "React", "PHP", "Python", "GCP", "Django","CodeIgniter","Laravel"],
+  projects: [
+
+
+    {
+      name: "E-Ration Shop System Website",
+      description:
+        "Developed an E-Ration shop web application which is interactive and attractive. Users can easily create an account and get an E-ration card, then additionally provide notification for stockupdates.",
+      link: "https://youtu.be/Aub0R-DABWM?si=4mlvVIqGy40DG8mC",
+      skills: ["Core PHP", "HTML", "CSS" , "JS" , "Bootstrap" , "Jquery" , "Ajax" ,"Mysql"],
+    },
+
+
+
+
+    {
+      name: "Online Billing System Website",
+      description:
+        "Developed an Online Billing System web application that is efficient and user-friendly. The system allows businesses to easily generate, manage, and track customer bills digitally. It includes features for automated invoice creation.",
+      link: "https://youtu.be/nkOHLk2Bh2c?si=r6s-5hjOf-118ULN",
+      skills: ["Core PHP", "HTML", "CSS" , "JS" , "Bootstrap" , "Jquery" , "Ajax","Mysql"],
+    },
+
+
+     {
+      name: "Web Hosting in GCP",
+      description:
+        "Developed an E-commerce website which enables users to order products and bill check etc Deployment of cloud by using services like compute engine,  VM instance and Ubuntu interface.",
+        
+
+      link: "https://www.linkedin.com/posts/aravinth-webdeveloper_gcp-aws-azure-activity-7220797915370246144-Cde0?utm_source=share&utm_medium=member_desktop",
+      skills: ["GCP", "Linux","Core PHP", "HTML", "CSS" , "JS" , "Bootstrap" , "Jquery" , "Ajax","Mysql"],
+    },
+
+    {
+      name: "Arudhras jewels Website",
+      description:
+        "At Arudhras Jewellery, they provides blend exquisite craftsmanship with premium materials to create stunning pieces that last a lifetime and their dedication to detail ensures that every jewel is a masterpiece of elegance and enduring beauty.",
+      link: "https://arudhrasjewels.com/",
+      skills: ["Wordpress","Woocommerce"],
+    },
+
+
+
+
+
+
+
+
+    {
+      name: "Cake Heist Website",
+      description:
+        "At Cakeheist, quality is top priority. Every item is crafted with the finest, fresh ingredients, ensuring a light, moist, and flavorful experience with every single bite. their pride ourselves on attention to detail and delivering exceptional products that bring joy and a smile to our customers' faces. they're not just serving desserts we're crafting unforgettable experiences, one slice at a time.",
+      link: "https://cakeheist.in/",
+      skills: ["Wordpress","Woocommerce"],
+    },
+
+
+    
+  ],
+  experience: [
+    {
+      company: "Victopia labs private limited",
+      title: "Full stack developer",
+      dateRange: "May 2025 - july 2025",
+      bullets: [
+        "During my internship at Victopia labs, I learned about Python(Django) to build the backend and its value,then I worked in their own web product and provided valued content for the site.",
+      ],
+    },
+    {
+      company: "Blackben technology private limited",
+      title: "Wordpress Developer",
+      dateRange: "Aug 2024 - Oct 2024",
+      bullets: [
+        "During my internship at Blackben technology private limited, I learned about Wordpress to build the websites and web application and  provided valued contribution to their client projects.",
+      ],
+    },
+    {
+      company: "NextGen Cloud Pro Learning Hub",
+      title: "Cloud computing intern",
+      dateRange: "Sep 2024 - Oct 2024",
+      bullets: [
+        "During my internship at NextGen Cloud Pro Learning Hub, I learned about Cloud computing to build the websites and deploy it into the webserver through GCP Ubuntu Linux.",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "The Madura College , Madurai",
+      degree: "Bachelor of Science in Information Technology",
+      dateRange: "2022 - 2025",
+      achievements: [
+        "Passout with 8.0 GPA",
+        "Attend many intercollegiate events",
+      ],
+    },
+    {
+      school: "Pathi Computer Education Institute",
+      degree: "Full Stack Web Development Certificate",
+      dateRange: "2022",
+      achievements: [
+        "Completed many real time projects",
+        "Built own portfolio website",
+        "Specialized in CorePHP and Web design",
+      ],
+    },
+    
+  ],
+};
